b013f1545aa8b26a80a0deae5db74f30ec040e80
jasperchan5 <jscnn51011@gmail.com>
2022-12-14 19:30:29 +0800

    endgame logic

--- gobang.py
+++ gobang.py
@@ -22,7 +22,9 @@ class Board:
     # Initialize the court
     def __init__(self):
         pygame.init()
-        self.court = np.empty((15,15), dtype=object)
+        self.court = np.zeros((15,15), dtype=object)
+        self.len = 15
+        self.width = 15
         self.backgroundColor = "#cdcfd0"
 
     def initGame(self, resolution): # Create a blank game interface
@@ -53,12 +55,12 @@ class Board:
             # Display chess
             # Coord argument is a tuple of (x, y)
             newChess = Chess(gameScreen, color, displayedPos)
-            print(newChess.centerCoord)
+            # print(newChess.centerCoord)
             pygame.draw.circle(newChess.gameScreen, newChess.color, newChess.centerCoord, newChess.radius)
-            if color == (0,0,0):
-                self.court[coord[0]][coord[1]] = 1
+            if color == (0, 0, 0):
+                self.court[coord[1]][coord[0]] = 1
             else:
-                self.court[coord[0]][coord[1]] = 0
+                self.court[coord[1]][coord[0]] = 0
             pygame.display.update()
     
     # To avoid location mistake
@@ -77,7 +79,33 @@ class Board:
     
     def endGame(self):
         # Pass board information to server and retrieve who wins and who loses
-        pass
+        for i in range(self.len):
+            for j in range(self.width):
+                try:
+                    if self.court[i][j] == 1 and self.court[i+1][j] == 1 and self.court[i+2][j] == 1 and self.court[i+3][j] == 1:
+                        print("vertical")
+                        return True
+                except:
+                    pass
+                try:
+                    if self.court[i][j] == 1 and self.court[i][j+1] == 1 and self.court[i][j+2] == 1 and self.court[i][j+3] == 1:
+                        print("horizontal")
+                        return True
+                except:
+                    pass
+                try:
+                    if self.court[i][j] == 1 and self.court[i+1][j+1] == 1 and self.court[i+2][j+2] == 1 and self.court[i+3][j+3] == 1:
+                        print("slant right")
+                        return True
+                except:
+                    pass
+                try:
+                    if self.court[i][j] == 1 and self.court[i+1][j-1] == 1 and self.court[i+2][j-2] == 1 and self.court[i+3][j-3] == 1:
+                        print("slant left")
+                        return True
+                except:
+                    pass
+        return False
     
 class ResultPage:
     def __init__(self):
--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame,sys
 from pygame.locals import QUIT
-from gobang import Board
+from gobang import Board, StartPage
 
 game = Board()
-resolution = (620,620)
+resolution = (620, 620)
 screen = game.initGame(resolution)
-pos = (100,100)
-botPos = (100,100)
-black = (0,0,0)
-white = (255,255,255)
+pos = (100, 100)
+botPos = (100, 100)
+colors = ["#000000", "#000080", "#008000", "#ff0000",  "#800080", "#00ff00", "#00ffff"]
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # Use "your turn", "not your turn" to determine current player
             pos = pygame.mouse.get_pos()
             game.displayChess(screen, pos, black)
-            if game.endGame() == "Black wins":
-                pygame.quit()
-                sys.exit()
+            if game.endGame():
+                # pygame.quit()
+                # sys.exit()
+                game = Board()
+                game.initGame(resolution)
         elif event.type == QUIT:
             pygame.quit()
             sys.exit()
